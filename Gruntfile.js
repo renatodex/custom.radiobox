959cd6cdbeca2f58e2eac8c326b1c6fb6e18d3e4
Renato Alves <renatodex@gmail.com>
2015-06-19 08:45:19 -0300

    Added basic setup

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    uglify: {
+       bower: {
+        options: {
+          mangle: false,
+          compress: true
+        },
+        files: {
+          'dist/bridger.min.js': 'src/bridger.js'
+        }
+      }
+    },
+    jasmine: {
+      pivotal: {
+        src: 'src/**/*.js',
+        options: {
+          specs: 'spec/*spec.js',
+          helpers: 'spec/*helper.js',
+          vendor: [
+            "bower_components/jquery/dist/jquery.js",
+            "bower_components/bridger/dist/bridger.min.js",
+            "bower_components/jasmine-jquery/lib/jasmine-jquery.js",
+          ]
+        }
+      }
+    }
+  });
+
+  // Register tasks.
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Task defining.
+  grunt.registerTask('default', 'jasmine');
+  grunt.registerTask('build', ['uglify']);
+};
